0df63bcd21efb2dd978fb5c1958ff7780a9cf38a
joshua_zapusek <53499306+joshuazapusek@users.noreply.github.com>
2020-09-28 15:57:35 -0400

    Add files via upload

--- apply_imnormalize.m
+++ apply_imnormalize.m
@@ -1,3 +1,11 @@
+%{
+Description: A function to normalize the input image
+
+Inputs: Array size NxMx3
+
+Outputs: Array size NxMx3
+%}
+
 function outarray = apply_imnormalize(inarray)
     outarray = inarray(:,:,:,1)/255.0 -.5;
 end
