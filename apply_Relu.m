479bf73081c1f99c7629cc88252e293cc30682bc
jochuchemon7 <37346269+jochuchemon7@users.noreply.github.com>
2020-09-28 19:34:53 -0400

    Update apply_Relu.m

--- apply_Relu.m
+++ apply_Relu.m
@@ -1,14 +1,16 @@
 format compact
 
 %{
+Description: Relu replaces the negative values with 0 value
+
 Input: 32x32x10, 16x16x10 or 8x8x10
 Output: 32x32x10, 16x16x10 or 8x8x10
 %}
 
-%Relu replaces the negative values with 0 value
+
 function result = relu(image)
 
-out = max(image,0)
+out = max(image,0) 
 result = out;
 
 end
