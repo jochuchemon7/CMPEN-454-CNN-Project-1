1fa15e43769a18397035d20fe2fb42ca7b861e76
joshua_zapusek <53499306+joshuazapusek@users.noreply.github.com>
2020-09-28 15:56:50 -0400

    Add files via upload

--- apply_relu.m
+++ apply_relu.m
@@ -1,3 +1,12 @@
+%{
+Description: A function for rectified linear unit, where all negative
+numbers are converted to 0.
+
+Inputs: Array size NxMxD
+
+Outputs: Array size NxMxD
+%}
+
 function outarray = apply_relu(inarray)
     outarray = max(inarray(:,:,:,1),0);
 end
